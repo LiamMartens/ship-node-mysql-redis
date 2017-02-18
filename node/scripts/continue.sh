ed2af003d6638ed8eb0471aae7935dbf9befcc4e
Liam Martens <hi@liammartens.com>
2017-02-18 16:53:25 +0100

    Fixed node permissions

--- node/scripts/continue.sh
+++ node/scripts/continue.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 export HOME=/home/www-data
 
+npm config set prefix /home/www-data/.node_modules
+export PATH=/home/www-data/.node_modules/bin:$PATH
+
+
 # set timezone
 cat /usr/share/zoneinfo/$TIMEZONE > /etc/localtime
 echo $TIMEZONE > /etc/timezone
